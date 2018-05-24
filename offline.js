b5d81832c34cecd95f985ea8e23b5f0031d6a930
mavieirapm <30505889+mavieirapm@users.noreply.github.com>
2018-05-24 17:23:21 -0300

    Add files via upload

--- offline.js
+++ offline.js
@@ -1,5 +1,5 @@
 ﻿{
-	"version": 1527188781,
+	"version": 1527193277,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
@@ -39,6 +39,9 @@
 		"images/relatomotivotudo-sheet0.png",
 		"images/sprite14-sheet0.png",
 		"images/sprite15-sheet0.png",
+		"images/sprite18-sheet0.png",
+		"images/sprite19-sheet0.png",
+		"images/sprite20-sheet0.png",
 		"icon-16.png",
 		"icon-32.png",
 		"icon-114.png",
